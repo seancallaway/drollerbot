de23a4dfe0a54434dbeba066ae1f831302f6dedb
Sean Callaway <seancallaway@gmail.com>
2017-05-15 13:24:07 -0700

    Added formatting and some string sanitization.

--- drollerbot/bot.py
+++ drollerbot/bot.py
@@ -5,30 +5,45 @@ from .die import Die
 thebot = Bot(command_prefix=".")
 
 def do_roll(commands):
+    """Perform the roll calculations."""
+    
     result = ""
+    "".join(commands.split())
+    commands = commands.lower()
 
     try:
+        mod = 0
         (numdice, remainder) = commands.split('d', 1)
         if '-' in remainder:
             (diesize, modifier) = remainder.split('-', 1)
+            mod = -1
         elif '+' in remainder:
             (diesize, modifier) = remainder.split('+', 1)
+            mod = 1
         else:
             diesize = int(remainder)
             modifier = 0
+
         numdice = int(numdice)
         diesize = int(diesize)
-        modifier = int(modifier)
+        modifier = int(modifier) * mod
 
         total = 0
 
         for i in range(numdice):
             temp = Die(diesize)
             roll = temp.roll()
+            result = result + ' (' + str(roll) + ') '
+            if i < numdice - 1:
+                result = result + "+"
             total += roll
 
-        result = total + modifier
+        if modifier < 0:
+            result = result + " - " + str(abs(modifier))
+        elif modifier > 0:
+            result = result + " + " + str(modifier)
 
+        result = result + " = [" + str(total+modifier) + ']'
 
     except ValueError:
         result = "Usage: .roll #d#(+modifier)"
