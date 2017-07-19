53514e55c650d4cd003efd2b2f11b5dec848b1e1
Sean Callaway <seancallaway@gmail.com>
2017-07-19 09:28:49 -0700

    Added 8ball for #6.

---
+++ drollerbot/8ball.py
@@ -0,0 +1,45 @@
+"""
+    8ball.py
+    ~~~~~~~~
+
+    Exposes the `8Ball` object
+
+    :copyright: (c) 2017 by Sean Callaway.
+    :license: MIT, see LICENSE for more details.
+"""
+import random
+
+class 8Ball(object):
+    """Defines a magic 8 ball."""
+
+    def __init__(self):
+        """Setup the 8ball with responses."""
+        random.seed()
+        self.responses = [
+                           'It is certain.', 
+                           'It is decidedly so.',
+                           'Without a doubt.',
+                           'Yes, definitely.',
+                           'You may rely on it.',
+                           'As I see it, yes.',
+                           'Most likely.',
+                           'Outlook good.',
+                           'Yes.',
+                           'Signs point to yes.',
+
+                           'Reply hazy. Try again.',
+                           'Ask again later',
+                           'Better not tell you now.',
+                           'Cannot predict now.',
+                           'Concentrate and ask again.',
+
+                           'Don\'t count on  it.',
+                           'My reply is no.',
+                           'My sources say no.',
+                           'Outlook not so good.',
+                           'Very doubtful.',
+                         ]
+
+    def shake(self):
+        """Shake the 8ball and get a response."""
+        return self.responses[random.randint(1, len(self.responses))]
